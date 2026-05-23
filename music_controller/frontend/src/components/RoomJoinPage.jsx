6fe179e110a0600a70dd659cf67e66d6185360a4
Qenszu <Opalusaktv@gmail.com>
2026-05-23 22:53:36 +0200

    Create Join Room window

--- music_controller/frontend/src/components/RoomJoinPage.jsx
+++ music_controller/frontend/src/components/RoomJoinPage.jsx
@@ -1,5 +1,74 @@
 import React from "react";
+import { useState } from "react";
+import { TextField, Grid, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function RoomJoinPage() {
-  return <p>This is join room page</p>;
+  const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  function handleTextFieldChange(e) {
+    setRoomCode(e.target.value);
+  }
+
+  function handleRoomButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code: roomCode,
+      }),
+    };
+
+    fetch("/api/join-room", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          navigate(`/room/${roomCode}`);
+        } else {
+          setError("Room not found");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <Grid container spacing={2} direction="column" alignItems="center">
+      <Grid item xs={12}>
+        <Typography variant="h4" component="h4">
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          error={error}
+          label="Code"
+          placeholder="Enter a Room Code"
+          value={roomCode}
+          helperText={error}
+          variant="outlined"
+          onChange={handleTextFieldChange}
+        />
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleRoomButtonPressed}
+        >
+          Enter Room
+        </Button>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button variant="contained" color="primary" to="/" component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
