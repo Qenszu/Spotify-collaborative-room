b27edcfbfc8f600026243e5c97826763352ed116
Qenszu <Opalusaktv@gmail.com>
2026-05-23 23:40:31 +0200

    Set session configuration

--- music_controller/frontend/src/components/HomePage.jsx
+++ music_controller/frontend/src/components/HomePage.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
 
 export default function HomePage() {
+  const [roomCode, setRoomCode] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        setRoomCode(data.code);
+      });
+  }, []);
+
+  function renderHomePage() {
+    return (
+      <Grid container spacing={3} direction="column" alignItems="center">
+        <Grid item xs={12}>
+          <Typography variant="h3" component="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<p>This is the home page</p>} />
+        <Route path="/" element={renderHomePage()} />
         <Route path="/join" element={<RoomJoinPage />} />
         <Route path="/create" element={<CreateRoomPage />} />
         <Route path="/room/:roomCode" element={<Room />} />
